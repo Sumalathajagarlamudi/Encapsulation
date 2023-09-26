10a02ae6e061f4f6d4b934e65675025627eb4289
sumalatha <jagarlamudisuma@gmail.com>
2023-09-27 00:20:30 +0530

    Encapsulation

---
+++ encapstopic/Encapsulation.java
@@ -0,0 +1,21 @@
+package encapstopic;
+
+public class Encapsulation {
+
+		 private String name ="suma";
+		 private int num = 535;
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public int getNum() {
+			return num;
+		}
+		public void setNum(int num) {
+			this.num = num;
+		}
+	}
+
+
---
+++ encapstopic/NewClass.java
@@ -0,0 +1,18 @@
+package encapstopic;
+
+public class NewClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Encapsulation obj=new Encapsulation();
+		System.out.println(obj.getName());
+		obj.setName("muskan");
+		System.out.println(obj.getName());
+		System.out.println(obj.getNum());
+		obj.setNum(200);
+		System.out.println(obj.getNum());
+		
+
+	}
+
+}
